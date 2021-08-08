82b7af0be4418ed6c9e7931dfcdbb6a3bb18bcec
Oliver Siegmar <oliver@siegmar.de>
2021-08-08 23:23:06 +0200

    cleanup client after test

--- src/test/java/de/siegmar/billomat4j/client/ClientServiceIT.java
+++ src/test/java/de/siegmar/billomat4j/client/ClientServiceIT.java
@@ -211,6 +211,7 @@ public class ClientServiceIT {
         assertNotNull(client.getId());
         assertEquals(client.getSepaMandate(), sepaMandateNr);
         assertEquals(client.getSepaMandateDate(), sepaDate);
+        createdClients.add(client);
     }
 
 }
